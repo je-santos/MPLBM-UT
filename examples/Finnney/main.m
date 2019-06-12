2ffe3171d83a907460c5c3dd8015022ff5d56043
Javier E Santos <jesantos@utexas.edu>
2019-06-11 23:05:35 -0600

    m

--- examples/Finnney/main.m
+++ examples/Finnney/main.m
@@ -4,9 +4,11 @@ f1 = fopen('spheres_a10_dx0.04_n500_segmented_unsigned_char.raw','r'); %read raw
 fp = fread(f1, d_size*d_size*d_size,'uint8=>uint8');
 fp = reshape(fp, d_size,d_size,d_size);
 fp = eliminate_isolatedRegions(fp, connect);
+fp = fp(0:100,0:100, 0:100); %subsamples the geometry into 100^3
 
 %% print for palabos
 
 % call a function to get the geom printed in this folder
 % i.e.
 % printPalabos(fp,'nameForTheFile')
+% if you can get an imageSC of the process, that would be great :)
