6a8fbac73b44fc4310d11c8ea497f544f5dabec5
Abhishek Bihani <abhishekdbihani@gmail.com>
2019-06-12 14:34:25 -0500

    Add files via upload
    Modified function to create input for palabos

--- examples/Finnney/main.m
+++ examples/Finnney/main.m
@@ -3,12 +3,9 @@ d_size = 500;
 f1 = fopen('spheres_a10_dx0.04_n500_segmented_unsigned_char.raw','r'); %read raw file
 fp = fread(f1, d_size*d_size*d_size,'uint8=>uint8');
 fp = reshape(fp, d_size,d_size,d_size);
-fp = eliminate_isolatedRegions(fp, connect);
-fp = fp(0:100,0:100, 0:100); %subsamples the geometry into 100^3
-
+%fp = eliminate_isolatedRegions(fp, connect);
+name='spheres';
 %% print for palabos
 
 % call a function to get the geom printed in this folder
-% i.e.
-% printPalabos(fp,'nameForTheFile')
-% if you can get an imageSC of the process, that would be great :)
+[status]=create_LBM_MAT2DAT(fp,name);
