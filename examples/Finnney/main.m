e66b238baabf03103032960cdc36441a5d982831
Javier E Santos <jesantos@utexas.edu>
2019-06-11 22:38:13 -0600

    finneys

---
+++ examples/Finnney/main.m
@@ -0,0 +1,12 @@
+addpath ('../../pre-processing') %pre-precesing libraries
+d_size = 500;
+f1 = fopen('spheres_a10_dx0.04_n500_segmented_unsigned_char.raw','r'); %read raw file
+fp = fread(f1, d_size*d_size*d_size,'uint8=>uint8');
+fp = reshape(fp, d_size,d_size,d_size);
+fp = eliminate_isolatedRegions(fp, connect);
+
+%% print for palabos
+
+% call a function to get the geom printed in this folder
+% i.e.
+% printPalabos(fp,'nameForTheFile')
