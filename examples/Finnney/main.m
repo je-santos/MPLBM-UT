351e17a851793847d8180a82a713f69fa68fed0a
Javier E Santos <jesantos@utexas.edu>
2019-06-22 10:28:57 -0600

    updated Finney example

--- examples/Finnney/main.m
+++ examples/Finnney/main.m
@@ -1,11 +1,14 @@
+%% creates a geometry for simulation
+
 addpath ('../../pre-processing') %pre-precesing libraries
-d_size = 500;
-f1 = fopen('spheres_a10_dx0.04_n500_segmented_unsigned_char.raw','r'); %read raw file
+d_size = 500; %voxels each side
+f1 = fopen('input/spheres_a10_dx0.04_n500_segmented_unsigned_char.raw','r'); %read raw file
 fp = fread(f1, d_size*d_size*d_size,'uint8=>uint8');
 fp = reshape(fp, d_size,d_size,d_size);
-%fp = eliminate_isolatedRegions(fp, connect);
-name='spheres';
-%% print for palabos
 
-% call a function to get the geom printed in this folder
-[status]=create_LBM_MAT2DAT(fp,name);
+connect = 6; % pixel connectivity 6, 18, 26
+fp = eliminate_isolatedRegions(fp, connect); %for better convergence
+
+%% print for palabos
+name='spheres4Palabos';
+[status] = mat2dat_4lbm(fp,name,1);
