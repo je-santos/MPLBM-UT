442c9b6e0f9cb7e6b8e692784b0e757f8fa84b72
Javier E. Santos <42117435+je-santos@users.noreply.github.com>
2020-01-29 13:28:13 -0600

    Add files via upload

--- examples/tortuosity_carbonate/get_geom_fromDRP.sh
+++ examples/tortuosity_carbonate/get_geom_fromDRP.sh
@@ -1,6 +1,4 @@
-!#bin/bash
-
-mkdir input
-cd input
-wget --no-check-certificate https://www.digitalrocksportal.org/projects/57/images/71283/download/
+mkdir input &&
+cd input &&
+wget --no-check-certificate https://www.digitalrocksportal.org/projects/57/images/71283/download/ &&
 mv index.html grid_25.bin
