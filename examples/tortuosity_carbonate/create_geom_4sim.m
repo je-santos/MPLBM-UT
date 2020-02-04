146117ee9181048d34e242cf5971ba29fb7e7496
Javier E. Santos <42117435+je-santos@users.noreply.github.com>
2020-02-04 00:44:37 -0600

    longer domain

--- examples/tortuosity_carbonate/create_geom_4sim.m
+++ examples/tortuosity_carbonate/create_geom_4sim.m
@@ -20,7 +20,7 @@ fp_printing = eliminate_isolatedRegions(fp_printing, connect); %for better conve
 %% making a computationally efficent domain for sim
 name = ['carbonate4Palabos'];
 add_mesh   = false;% add a neutral-wet mesh at the end of the domain
-num_slices = 0;    % add n empty slices at the beggining and end of domain 
+num_slices = 2;    % add n empty slices at the beggining and end of domain 
                    % for pressure bcs
 swapXZ = true;     % Swap x and z data if needed to ensure Palabos simulation in Z-direction              
 scale_2 = false;   % Double the grain (pore) size if needed to prevent single pixel throats
