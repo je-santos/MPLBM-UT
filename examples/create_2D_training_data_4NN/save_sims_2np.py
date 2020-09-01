d841e81ec65939bdb8191b40d739d51a92f31d54
Javier E. Santos <42117435+je-santos@users.noreply.github.com>
2020-09-01 12:45:09 -0500

    Add files via upload

--- examples/create_2D_training_data_4NN/save_sims_2np.py
+++ examples/create_2D_training_data_4NN/save_sims_2np.py
@@ -1,3 +1,4 @@
+import re
 import glob
 import numpy as np
 
@@ -9,9 +10,17 @@ y = []
 for sim_dir in all_dirs:
     sim = glob.glob(sim_dir+'/*.dat')
     if sim:
-        print('list')
-        y.append(np.loadtxt(sim[0]).reshape(3,258,128,3))
         
+        domain_num = re.findall(r'\d+',sim[0])[0]
+        domain = glob.glob('input/')
+        y.append(np.loadtxt(sim[0]).reshape(258,128,3,3)[1:-1,:,1,0].T) #Y,X,slice,vel
+        domain_loc = glob.glob(f'input/{domain_num}_data*')[0]
+        X.append(((np.loadtxt(domain_loc).reshape(258,128,3)[1:-1,:,1].T)>0)*1)
         
-        # the x is located in input
-    
+
+X_np = np.array(X).astype(np.int8)
+y_np = np.array(y).astype(np.float16)
+
+
+np.save('numpys/X',X_np)
+np.save('numpys/y',y_np)
