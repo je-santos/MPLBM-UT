fc0ad3791549060ee6031da37e8bd5dda52010d8
Abhishek Bihani <abhishekdbihani@gmail.com>
2020-09-02 12:41:49 -0500

    Added folder suggestions

--- examples/unsteady_relperm_spherepack/create_geoms_4_kr.m
+++ examples/unsteady_relperm_spherepack/create_geoms_4_kr.m
@@ -5,7 +5,7 @@ kr.domain_size  = [200,200,200];
 kr.mesh_added   = true; % was a neutral-wet mesh added at the end of the domain?
 kr.num_slices   = 4;    % how many n empty slices at the begining and end of domain
 kr.input_dir    = 'input';
-kr.input_geom   = 'spheres4Palabos';  %This is original geometry. Output geometries from 2-phase simulation should be put in another folder
+kr.input_geom   = 'spheres4Palabos';  %This is original geometry in input folder. Output geometries from 2-phase simulation should be put in another folder (eg. tmp)
 kr.output_file  = 'info_4kr.txt';
 kr.pressure_bcs = true;
 % if true, the following info is needed for the Pc-S curve
