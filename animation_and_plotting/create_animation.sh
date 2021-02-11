d1675611402baf1012e540bbd62fcd9552e3b2da
alexgigliotti <alex.gigliotti@utexas.edu>
2021-02-11 17:01:06 -0600

    Multithread plotting

--- animation_and_plotting/create_animation.sh
+++ animation_and_plotting/create_animation.sh
@@ -1,9 +1,27 @@
 start=`date +%s`
 
-for i in {0..1}
+run_pyvista_animation() {
+    j=$1
+    
+    #echo 'Process # ' $j
+    #sleep 3
+    python pyvista_lbm_animation.py $j
+}
+
+
+num_proc=4 #$(ulimit -u) this command get max number of processes, but limiting at 4 because memory 
+
+for i in {0..18}
 do
-    python pyvista_lbm_animation.py $i
+    j=$i #keep track of the for loop with j so i can be used for limiting processes
+    
+    ((i=i%num_proc)); ((i++==0)) && wait
+    
+    run_pyvista_animation "$j" &
+    
+    #echo 'Process # ' $j
 done
+wait
 
 python create_gif.py
 
