4543fa1f562a211313fa2a6026f04b32013ab52c
jesantos <jesantos@pn2002136.lanl.gov>
2021-10-21 14:45:35 -0600

    python works!!!!!

--- examples/pythonIO/create_geom.py
+++ examples/pythonIO/create_geom.py
@@ -1,58 +1,11 @@
+import sys
 from argparse import Namespace
 
 import numpy as np
-import matplotlib.pyplot as plt
-from skimage import measure
 
+sys.path.append('../../pre-processing') # I'll do this the proper way soon
+from pore_utils import *
 
-
-def create_geom_edist(rock, args):
-    from scipy.ndimage.morphology import distance_transform_edt as edist
-    
-    if args.swapXZ:
-        rock = rock.transpose([2, 1, 0])
-        
-    if args.scale_2:
-        NotImplementedError('Feature not yet implemented')
-
-    erock   = edist(rock)
-    
-    # make sure all the BCs have BB nodes
-    erock[0,:,:]  = 1
-    erock[:,0,:]  = 1
-    erock[:,:,0]  = 1
-    
-    erock[-1,:,:] = 1
-    erock[:,-1,:] = 1
-    erock[:,:,-1] = 1
-    
-    # re open the pores
-    erock[rock==0]=0;
-    
-    # Get the final matrix [0,1,2]
-    erock[(erock>0)*(erock<2)] = 1
-    erock[erock>1] = 2
-    
-    if args.add_mesh:
-        NotImplementedError('Feature not yet implemented')
-        
-    if args.num_slices:
-        erock = np.pad(erock,[(args.num_slices,args.num_slices),(0,0),(0,0)])
-        
-    
-    if args.print_size:
-        size = erock.shape
-        geom_name = f'{args.name}_{size[0]}_{size[1]}_{size[2]}'
-    else:
-        geom_name = args.name
-        
-    #np.savetxt(f'input/{geom_name}.dat', erock.flatten())
-    #with open(f'input/{geom_name}.dat', 'w') as file:
-    #    for i in range(erock.shape[0]):
-    #        file.write(erock[i,:,:])
-    #erock.astype('int8').tofile(f'input/{geom_name}.dat')
-    np.savetxt(f'input/{geom_name}.dat', erock.reshape([252,-1]), fmt='%i', newline='/n', delimiter=' ')
-    return erock
 
 
 
@@ -63,29 +16,19 @@ rock = np.fromfile('input/grid_25.bin', dtype='int8').reshape([480,480,480])/3
 subsize = 250
 rock    = rock[0:subsize,0:subsize,0:subsize]
 
-# find connected-comps
-blobs_labels = measure.label(rock, background=1, connectivity=1)
-
-#vols = [np.sum(blobs_labels==label) for label in range(np.max(blobs_labels))]
-# it seems that label 1 is the largest comp, but gotta check
-
-# delete non-connected regions
-rock[blobs_labels>1] = 0
-
-
+# geom inputs
 geom            = Namespace()
 geom.name       = 'carbonate4Palabos'
 geom.print_size = True
 geom.add_mesh   = False # add a neutral-wet mesh at the end of the domain
 geom.num_slices = 1     # add n empty slices at the beggining and end of domain 
                         # for pressure bcs
-geom.swapXZ     = True    # Swap x and z data if needed to ensure Palabos simulation in Z-direction              
-geom.scale_2    = False   # Double the grain (pore) size if needed to prevent single pixel throats
-                          # for tight/ low porosity geometries                   
+geom.swapXZ     = True  # Swap x and z data if needed to ensure Palabos simulation in Z-direction              
+geom.scale_2    = False # Double the grain (pore) size if needed to prevent single pixel throats
+                        # for tight/ low porosity geometries                   
 
+rock     = erase_regions(rock)
 rock4sim = create_geom_edist(rock,geom) # provides an efficient geometry for simulation
-
-
 
 
 
