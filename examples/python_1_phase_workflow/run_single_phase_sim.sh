295d2d0c30c8e90a3294d807887d664d16d8f574
Alex Gigliotti <42842519+alexgigliotti@users.noreply.github.com>
2021-11-23 17:03:46 -0600

    Update run_single_phase_sim.sh

--- examples/python_1_phase_workflow/run_single_phase_sim.sh
+++ examples/python_1_phase_workflow/run_single_phase_sim.sh
@@ -7,4 +7,4 @@ wget www.digitalrocksportal.org/projects/65/images/71108/download/ -O input/rg_t
 python ../../python_utils/workflow_run_1_phase_sim.py input.yml
 
 # Run single phase simulation
-bash input/run_single_phase_sim.sh
+bash input/run_single_phase_sim.sh > sim_log.txt
