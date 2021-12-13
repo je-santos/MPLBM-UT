28d8bc4e01394000e51a28aad968edeab6abacff
Alex Gigliotti <42842519+alexgigliotti@users.noreply.github.com>
2021-12-13 15:55:01 -0600

    Add example rel perm script

---
+++ examples/python_2_phase_workflow/run_rel_perm.sh
@@ -0,0 +1,5 @@
+# Run single phase python workflow here
+python3 ../../python_utils/workflow_run_relperm_sim.py input.yml
+
+# Run two-phase simulation
+bash input/run_relperm_sim.sh #  > relperm_log.txt
--- examples/python_2_phase_workflow/run_two_phase_sim.sh
+++ examples/python_2_phase_workflow/run_two_phase_sim.sh
@@ -4,7 +4,7 @@
 wget www.digitalrocksportal.org/projects/65/images/71108/download/ -O input/rg_theta60_phi10.raw
 
 # Run single phase python workflow here
-python ../../python_utils/workflow_run_2_phase_sim.py input.yml
+python3 ../../python_utils/workflow_run_2_phase_sim.py input.yml
 
 # Run two-phase simulation
-bash input/run_shanchen_sim.sh > sim_log.txt
+bash input/run_shanchen_sim.sh  > shanchen_log.txt
