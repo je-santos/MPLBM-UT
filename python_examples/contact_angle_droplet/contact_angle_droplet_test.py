4c6a5772be18db13d9fa49f76e89e3a021bea5e9
Alex Gigliotti <42842519+alexgigliotti@users.noreply.github.com>
2022-02-18 16:21:04 -0600

    Update droplet example

--- python_examples/contact_angle_droplet/contact_angle_droplet_test.py
+++ python_examples/contact_angle_droplet/contact_angle_droplet_test.py
@@ -11,16 +11,19 @@ from parse_input_file import *
 
 def create_flat_surface(inputs):
 
-    domain = np.zeros((75, 75, 75), dtype=np.uint8)  # empty space
+    domain = np.zeros((80, 80, 80), dtype=np.uint8)  # empty space
     domain[0:5,:,:] = 1  # surface
     # domain = np.transpose(domain, [1,0,2])
     domain.tofile(f"{inputs['input output']['input folder']}/flat_surface.raw")
     # print(domain.shape)
 
-    import vedo as vd
-    vp = vd.Plotter(axes=3, bg='w', bg2='w', size=(1200, 900), offscreen=False)
-    domain = vd.Volume(domain).isosurface(0.5)
-    vp += domain.c('gray')
+    print('Creating efficient geometry for Palabos...')
+    create_geom_for_palabos(inputs)
+
+    # import vedo as vd
+    # vp = vd.Plotter(axes=3, bg='w', bg2='w', size=(1200, 900), offscreen=False)
+    # domain = vd.Volume(domain).isosurface(0.5)
+    # vp += domain.c('gray')
     # vp.show()
 
     return
@@ -28,23 +31,18 @@ def create_flat_surface(inputs):
 
 def run_2_phase_sim(inputs):
     # Steps
-    # 1) create geom for palabos
-    # 2) create palabos input file
-    # 3) run 2-phase sim
+    # 1) create palabos input file
+    # 2) run 2-phase sim
 
     if inputs['simulation type'] == '1-phase':
         raise KeyError('Simulation type set to 1-phase...please change to 2-phase.')
     sim_directory = inputs['input output']['simulation directory']
 
-    # 1) Create Palabos geometry
-    print('Creating efficient geometry for Palabos...')
-    create_geom_for_palabos(inputs)
-
-    # 2) Create simulation input file
+    # 1) Create simulation input file
     print('Creating input file...')
     create_palabos_input_file(inputs)
 
-    # 3) Run 2-phase simulation
+    # 2) Run 2-phase simulation
     print('Running 2-phase simulation...')
     num_procs = inputs['simulation']['num procs']
     input_dir = inputs['input output']['input folder']
@@ -63,5 +61,13 @@ input_file = 'input.yml'
 inputs = parse_input_file(input_file)  # Parse inputs
 inputs['input output']['simulation directory'] = os.getcwd()  # Store current working directory
 create_flat_surface(inputs)
-run_2_phase_sim(inputs)  # Run 2 phase sim
+
+G_ads_f1_s1 = np.array([-0.4, 0, 0.4])  # G_ads_f1_s1 values corresponding to 156 deg, 90 deg, and 24 deg contact angles
+tmp_folders = ['tmp_m0.4/', 'tmp_0/', 'tmp_p0.4/']
+for i in range(len(G_ads_f1_s1)):
+    inputs['input output']['output folder'] = tmp_folders[i]
+    inputs['simulation']['fluid data']['G_ads_f1_s1'] = G_ads_f1_s1[i]
+    print(f"\n\nRunning droplet test with G_ads_f1_s1 = {G_ads_f1_s1[i]}...")
+    run_2_phase_sim(inputs)  # Run 2 phase sim
+
 
--- python_examples/contact_angle_droplet/droplet_viz.py
+++ python_examples/contact_angle_droplet/droplet_viz.py
@@ -50,11 +50,31 @@ def visualize_fluid(inputs, rho_file):
 
     f1_mesh = pv.read(rho_file)
     f1_density = f1_mesh.get_array('Density').reshape([nz, ny, nx+n_slices*2])
-    f1_density = f1_density[:, :, n_slices:nx - n_slices]
-    f1 = vd.Volume(f1_density).isosurface(1.5, largest=True)  # NW fluid
-    f2 = vd.Volume(f1_density).isosurface(0.5, largest=True)  # W fluid
+    f1_density = f1_density[:, :, n_slices:nx - 1]
+    f1 = vd.Volume(f1_density).isosurface(1.5, largest=False)  # NW fluid
+    f2 = vd.Volume(f1_density).isosurface(0.5, largest=False)  # W fluid
 
     return f1, f2
+
+
+def calculate_droplet_curvature(inputs, rho_file):
+
+    nx = inputs['domain']['domain size']['nx']
+    ny = inputs['domain']['domain size']['ny']
+    nz = inputs['domain']['domain size']['nz']
+    n_slices = inputs['domain']['inlet and outlet layers']
+
+    f1_mesh = pv.read(rho_file)
+    f1_mesh_subset = f1_mesh.extract_subset([n_slices, nx-n_slices-1, 0, ny-1, 0, nz-1])
+    f1_contour = f1_mesh_subset.contour(isosurfaces=[1.5], scalars='Density')
+    curvature_array = f1_contour.curvature(curv_type='Mean', progress_bar=False)
+    mean_curvature = np.mean(curvature_array)
+
+    # p = pv.Plotter()
+    # p.add_mesh(f1_contour, color='red')
+    # p.show()
+
+    return mean_curvature
 
 
 # Get inputs
@@ -62,19 +82,35 @@ input_file = 'input.yml'
 inputs = parse_input_file(input_file)  # Parse inputs
 inputs['input output']['simulation directory'] = os.getcwd()  # Store current working directory
 
+# Choose simulation to visualize
+which_sim = 2
+G_ads_f1_s1_labels = ['m0.4', '0', 'p0.4']
+G_ads_f1_s1_list = np.array([-0.4, 0, 0.4])
+tmp_folders = ['tmp_m0.4/', 'tmp_0/', 'tmp_p0.4/']
+inputs['input output']['output folder'] = tmp_folders[which_sim]
+
 # Get density files
 rho_files_list = get_rho_files(inputs)
 
+# Calculate mean curvature
+index = -1  # Choose last simulation output
+mean_curvature = calculate_droplet_curvature(inputs, rho_file=rho_files_list[index])
+print(mean_curvature)
+
 # Setup plotter
-vp = vd.Plotter(axes=3, bg='w', bg2='w', size=(1200,900), offscreen=False)
+vp = vd.Plotter(axes=0, bg='w', bg2='w', size=(1200,900), offscreen=False)
 # visualize medium
 grains = visualize_medium(inputs)
-vp += grains.c('gray')
+vp += grains.lighting('glossy').phong().c('seashell')
 # visualize fluids
-index = -1
 f1, f2 = visualize_fluid(inputs, rho_file=rho_files_list[index])
-vp += f1.c('red')
-# vp += f2.c('blue')
+vp += f1.subdivide(3).smooth().lighting('glossy').phong().c('lightblue')
+vp += vd.shapes.Text2D(f'G_ads_f1_s1 = {G_ads_f1_s1_list[which_sim]}\nMean Curvature = {np.round(mean_curvature, decimals=3)}', pos='top-left')
+cam = dict(pos=(90.66, -90.22, 73.85),
+           focalPoint=(36.60, 37.96, 5.068),
+           viewup=(-0.05450, 0.4543, 0.8892),
+           distance=155.2,
+           clippingRange=(46.16, 282.1))
 
-vp.show()
+vp.show(camera=cam).screenshot(f'droplet_viz_{G_ads_f1_s1_labels[which_sim]}.png', scale=1)
 
